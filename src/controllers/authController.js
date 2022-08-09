d208262fef0cd2c7588d767f6e7bc0245188ac5c
Lucas Lafèré <laferelucas@gmail.com>
2022-08-09 17:07:01 -0300

    added validation to signup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,18 @@
 import connection from '../db/database.js';
-import bcrypt, { compareSeync } from 'bcrypt';
+import bcrypt, { compareSync } from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 import { signUpSchema } from '../schemas/authSchema.js';
 
 export async function signUp (req, res) {
     const newUser = req.body;
     const { error } = signUpSchema.validate(newUser);
+    if (error) return res.status(422).send(error.message);
+    try {
+        
+        return res.sendStatus(201);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
 }
 export async function login (req, res) {
     const userLogin = req.body;
