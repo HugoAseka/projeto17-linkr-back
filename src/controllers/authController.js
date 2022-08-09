a50028468521c34383851f8b1c4febd964577d14
Lucas Lafèré <laferelucas@gmail.com>
2022-08-09 17:21:34 -0300

    signUp route fully works

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,19 @@ export async function signUp (req, res) {
     const { error } = signUpSchema.validate(newUser);
     if (error) return res.status(422).send(error.message);
     try {
-        
+            const checkExistingUser = await connection.query(`
+            SELECT * FROM users
+            WHERE email = $1;
+            `, [newUser.email]);
+            if (checkExistingUser.rowCount > 0) return res.status(409).send("Esse email está em uso");
+    
+            const passwordHash = bcrypt.hashSync(newUser.password, 10);
+    
+            await connection.query(`
+            INSERT INTO users
+            (username, email, password, "profilePhoto")
+            VALUES ($1, $2, $3, $4);
+            `, [newUser.username, newUser.email, passwordHash, newUser.profilePhoto]);
         return res.sendStatus(201);
     } catch (error) {
         return res.sendStatus(500);
