085d208887e8d0408ee79a7f442c9efd040da7e7
Lucas Lafèré <laferelucas@gmail.com>
2022-08-09 17:04:55 -0300

    added signup schema

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,11 @@
 import connection from '../db/database.js';
 import bcrypt, { compareSeync } from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+import { signUpSchema } from '../schemas/authSchema.js';
 
 export async function signUp (req, res) {
-    const user = req.body;
+    const newUser = req.body;
+    const { error } = signUpSchema.validate(newUser);
 }
 export async function login (req, res) {
     const userLogin = req.body;
