76b52452d3eeea4d4cd14aacf3a0bd04aac28bfe
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-08-10 19:53:58 -0300

    100%

--- src/routes/accessOtherUsersRoute.js
+++ src/routes/accessOtherUsersRoute.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import dotenv from "dotenv"; 
 import { getClickedUser,getUserByName } from "../controllers/accessOtherUsersController.js";
+import { checkAuth } from "../middlewares/authMiddleware.js";
 dotenv.config(); 
 
 const router = Router();
 
-router.get("/users/:id", getClickedUser);
+router.get("/users/:id", checkAuth, getClickedUser);
 router.get("/other_users",getUserByName);
 
 export default router;
