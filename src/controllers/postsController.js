eee9a680b87dc4f7cba5d1081019d422859960a8
HugoAseka <hugomaseka@gmail.com>
2022-08-11 12:16:12 -0300

    feat: url metadata information

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,6 @@
 import { postSchema } from "../schemas/postsSchema.js";
 import { postRepository } from "../repositories/postsRepository.js";
+import urlMetadata from "url-metadata";
 
 export async function getAllPosts(req, res) {
   const { rows: posts } = await postRepository.selectPosts();
@@ -7,14 +8,25 @@ export async function getAllPosts(req, res) {
 }
 
 export async function insertPost(req, res) {
-  const token = res.locals.token;
+  const Authorization = req.headers.authorization;
+  const token = Authorization?.replace("Bearer ", "");
   const newPost = req.body;
   const { error } = postSchema.validate(newPost);
-
+  let values = {};
   if (error) {
     return res.sendStatus(400);
   }
 
   await postRepository.createPost(token, newPost);
-  res.sendStatus(200);
+  
+  const x = urlMetadata(newPost.url).then(
+    function ({ title, image, description }) {
+      // success handler
+      res.status(200).json({ title, description, image });
+    },
+    function (error) {
+      // failure handler
+      return res.sendStatus(500);
+    }
+  );
 }
