e8c57c9f246cd7573ecb573a5702925d92ecf7ba
Igor <igorfontenelletexeira@gmail.com>
2022-08-12 17:46:26 -0300

    Getting the rank of hashtags

--- src/repositories/hashtagRepository.js
+++ src/repositories/hashtagRepository.js
@@ -13,6 +13,19 @@ async function getPostsByHashtags (hashtag) {
         `,[hashtag]);
 } 
 
+async function getHashtagRank () {
+    return connection.query(`
+    SELECT hashtags.id, hashtags.name, Count("hashtagsPosts"."hashtagId") AS count
+    FROM hashtags
+    JOIN "hashtagsPosts"
+    ON hashtags.id = "hashtagsPosts"."hashtagId"
+    GROUP BY hashtags.id
+    ORDER BY count DESC
+    LIMIT 10
+    `);
+}
+
 export const hashtagRepository = { 
-    getPostsByHashtags
+    getPostsByHashtags,
+    getHashtagRank
 }
--- src/routes/hashtagRouter.js
+++ src/routes/hashtagRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getHashtagPost } from '../controllers/hashtagController.js';
+import { getHashtagPost, getHashtagRanking } from '../controllers/hashtagController.js';
 import { checkAuth } from '../middlewares/authMiddleware.js';
 
 
 const hashtagRouter = Router();
 
 hashtagRouter.get("/hashtag/:hashtag", checkAuth, getHashtagPost);
+hashtagRouter.get("/ranking", getHashtagRanking);
 
 export default hashtagRouter;
