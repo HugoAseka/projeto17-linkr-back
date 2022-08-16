61a52d7d3c2017915db386be5e096d10f52bb00d
HugoAseka <hugomaseka@gmail.com>
2022-08-16 19:05:22 -0300

    update from main

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -5,7 +5,7 @@ import { checkAuth } from "../middlewares/authMiddleware.js";
 
 const postsRouter = Router();
 
-postsRouter.get("/posts",getAllPosts);
+postsRouter.get("/posts",checkAuth,getAllPosts);
 postsRouter.post("/posts", checkAuth, insertPost);
 postsRouter.put("/like/:id", updateLike);
 
